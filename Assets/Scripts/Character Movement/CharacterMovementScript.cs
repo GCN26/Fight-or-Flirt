8a27bd7195890ecb4b86590c139e5193562e3bf5
poopzilla <eearls@ltu.edu>
2026-01-27 16:11:16 -0500

    Character Controller made and working.

---
+++ Assets/Scripts/Character Movement/CharacterMovementScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CharacterMovementScript : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+    private Vector3 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.z = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+}
