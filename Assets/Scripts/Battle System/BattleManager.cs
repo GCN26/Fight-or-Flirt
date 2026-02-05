d5ea1f287fb3d299b193f7931a70ef3e6528eb9d
GibbyGCN26 <gibbysheldon@aol.com>
2026-02-05 16:02:28 -0500

    Attack System turn functionality
    Attack system now includes turns in a basic state. A proper visualization of the system has yet to be added.

--- Assets/Scripts/Battle System/BattleManager.cs
+++ Assets/Scripts/Battle System/BattleManager.cs
@@ -1,36 +1,113 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
-    public Combatant comb0, comb1;
+    public Combatant[] party;
+    public Combatant[] enemies;
+
+    int currentPartyIndex;
+
+    public List<Combatant> battleList;
+    public Coroutine battleCo;
+
     void Start()
     {
-        comb0.attack = 10;
-        comb0.defense = 5;
-        comb0.speed = 5;
-        comb0.hp = 20;
-        comb0.looks = 8;
-        comb0.intelligence = 10;
-        comb0.charisma = 15;
-
-        comb1.attack = 10;
-        comb1.defense = 5;
-        comb1.speed = 5;
-        comb1.hp = 20;
-        comb1.looks = 8;
-        comb1.intelligence = 5;
-        comb1.charisma = 1;
-        comb1.infatuation = 20;
-
-
+        
+        //battleCo = StartCoroutine(battleProcess());
     }
 
     public void testFight()
     {
-        comb0.attackEnemy(comb1);
+        //party[0].attackEnemy(enemies[0]);
+        battleCo = StartCoroutine(battleProcess());
     }
     public void testFlirt()
     {
-        comb0.rizzEnemy(comb1);
+        //party[0].rizzEnemy(enemies[0]);
+    }
+
+    IEnumerator battleProcess()
+    {
+        int partyDeadInt = 0;
+        foreach (Combatant comb in party) {
+            if(comb.hp > 0)
+            {
+                
+            }
+            else
+            {
+                partyDeadInt++;
+            }
+        }
+        if(party.Length == partyDeadInt)
+        {
+            Debug.Log("You lose!");
+            //End in Loss
+            StopCoroutine(battleCo);
+        }
+
+        int enemyDeadInt = 0;
+        foreach (Combatant comb in enemies)
+        {
+            if (comb.hp > 0 && comb.infatuation > 0)
+            {
+
+            }
+            else
+            {
+                enemyDeadInt++;
+            }
+        }
+        if (enemies.Length == enemyDeadInt)
+        {
+
+            Debug.Log("You win!");
+            //End in win
+            StopCoroutine(battleCo);
+        }
+
+        //Get Player Input
+        while (currentPartyIndex < party.Length)
+        {
+            if (party[currentPartyIndex].hp > 0)
+            {
+                battleList.Add(party[currentPartyIndex]);
+                currentPartyIndex++;
+            }
+        }
+        int enemyIndex = 0;
+        while(enemyIndex < enemies.Length)
+        {
+            if (enemies[enemyIndex].hp > 0)
+            {
+                battleList.Add(enemies[enemyIndex]);
+                enemyIndex++;
+            }
+        }
+
+        //Get Enemy Turn order
+        foreach (Combatant comb in battleList)
+        {
+            comb.attackEnemy(enemies[0]);
+        }
+
+        currentPartyIndex = 0;
+        battleList.Clear();
+        battleCo = StartCoroutine(battleProcess());
+        return null;
+    }
+
+    int checkSlots(int num)
+    {
+        if (num < 0) return -1;
+        if (party[num].hp > 0) return num;
+        else
+        {
+            if (num > 0) return checkSlots(num - 1);
+            else return -1;
+        }
     }
 }
--- Assets/Scripts/Battle System/Combatant.cs
+++ Assets/Scripts/Battle System/Combatant.cs
@@ -1,4 +1,7 @@
+using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [Serializable]
@@ -13,24 +16,70 @@ public class Combatant
     //Looks: Player only, essentially flirting attack
     //Intelligence: essentially flirting defense
     //Charisma: Player only, modifier for infatuation
+    public string charName;
     public int hp, infatuation, attack, defense, speed, looks, intelligence, charisma;
     int minAtk, minDef,minSpeed, minLooks, minInt, minRizz;
 
+    public int movePower;
+
     public int level = 1;
+
+    public List<Attack> attackList;
+
+    public enum status
+    {
+        Healthy,
+        Burned,
+        Confused,
+        Dead
+    }
 
     public void attackEnemy(Combatant enemy)
     {
         float crit = 1;
         int random = UnityEngine.Random.Range(0, 16);
         if (random == 0) crit = 1.75f;
-        int damage = (int)((attack*level)*(intelligence/3)*crit / (enemy.defense*enemy.level));
+        int damage = (int)((movePower*attack*level)*(intelligence/3)*crit / (enemy.defense*enemy.level));
         enemy.hp -= damage;
-        Debug.Log(damage);
+        Debug.Log(charName + " hits " + enemy.charName + " for " + damage.ToString());
     }
     public void rizzEnemy(Combatant enemy)
     {
-        int rizz = (int)(looks * (looks / enemy.intelligence));
+        int rizz = (int)((movePower * charisma) * looks * (looks / enemy.intelligence));
         enemy.infatuation -= rizz;
         Debug.Log(rizz);
+    }
+}
+
+public static class Attacks
+{
+    public static Attack[] attackList =
+    {
+        new Attack("Slash","Basic Sword Slash",15,0),
+        new Attack("Fire Slash","Firey Sword Slash",15,0),
+    };
+}
+
+[Serializable]
+public class Attack
+{
+    public string name;
+    public string desc;
+    public int power;
+    public enum AttackType
+    {
+        Fight,
+        Flirt,
+        Status
+    }
+    public AttackType type;
+    //Bonus Effects
+
+    public Attack(string name, string desc, int power, int type)
+    {
+        this.name = name;
+        this.desc = desc;
+        this.power = power;
+        this.type = (AttackType)type;
     }
 }
