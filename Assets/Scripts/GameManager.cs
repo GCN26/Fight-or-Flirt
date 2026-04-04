40129dd7a692a858a130288c9794453fe049d970
GibbyGCN26 <gibbysheldon@aol.com>
2026-04-04 10:41:13 -0400

    Small Change

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -48,10 +48,6 @@ public class GameManager : MonoBehaviour
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
 
     }
 
